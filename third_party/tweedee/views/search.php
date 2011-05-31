b868ad11d1cd2cc282126ad74fc11e8ab2330880
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-05-31 20:01:22 +0100

    Modify 'search builder' view to use Tweedee_criterion objects. Also move to using a table; no point fighting...

--- third_party/tweedee/views/search.php
+++ third_party/tweedee/views/search.php
@@ -1,59 +1,47 @@
+<?=form_open($form_action); ?>
 <div id="tweedee">
 
-<?=form_open($form_action); ?>
-	<p>Find Tweets matching the following criteria:</p>
-	
+<table class="mainTable padTable" cellpadding="0" cellspacing="0">
+    <thead>
+        <tr>
+            <th width="40%"><?=lang('thd_search_criterion_type'); ?></th>
+            <th><?=lang('thd_search_criterion_value'); ?></th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+
+    <tbody class="roland">
 	<?php
-		if ($search_criteria):
-		$criterion_count = 0;
-		foreach ($search_criteria AS $search_criterion):
+        if ( ! $search_criteria):
+    ?>
+        <tr class="row">
+            <td><?=form_dropdown('search_criteria[0][type]', $criterion_types); ?></td>
+            <td><?=form_input('search_criteria[0][value]', ''); ?></td>
+            <td class="act">
+                <a class="remove_row btn" href="#" title="<?=lang('lbl_remove_criterion'); ?>"><img src="<?=$theme_url; ?>/img/minus.png" /></a>
+                <a class="add_row" href="#" title="<?=lang('lbl_add_criterion'); ?>"><img src="<?=$theme_url; ?>/img/plus.png" /></a>
+            </td>
+        </tr>
+    <?php
+        else:
+        foreach ($search_criteria AS $criterion):
 	?>
-	<div class="tweedee_criterion">
-		<label>
-			<span><?=lang('lbl_search_criterion_type'); ?></span>
-			<?=form_dropdown("search_criteria[{$criterion_count}][type]", $criterion_types, $search_criterion['criterion_type']); ?>
-		</label>
-	
-		<label>
-			<span><?=lang('lbl_search_criterion_value'); ?></span>
-			<?=form_input("search_criteria[{$criterion_count}][value]", $search_criterion['criterion_value']); ?>
-		</label>
-
-		<div>
-			<a class="remove_criterion" href="#" title="<?=lang('lbl_remove_criterion'); ?>"><img src="<?=$theme_url; ?>/img/minus.png" /></a>
-			<a class="add_criterion" href="#" title="<?=lang('lbl_add_criterion'); ?>"><img src="<?=$theme_url; ?>/img/plus.png" /></a>
-		</div>
-	</div><!-- /.tweedee_criterion -->
-	<?php
-		$criterion_count++;
-		endforeach;
-		else:
-	?>
-	<div class="tweedee_criterion">
-		<label>
-			<span><?=lang('lbl_search_criterion_type'); ?></span>
-			<?=form_dropdown('search_criteria[0][type]', $criterion_types); ?>
-		</label>
-	
-		<label>
-			<span><?=lang('lbl_search_criterion_value'); ?></span>
-			<?=form_input('search_criteria[0][value]', ''); ?>
-		</label>
-
-		<div>
-			<a class="remove_criterion" href="#" title="<?=lang('lbl_remove_criterion'); ?>"><img src="<?=$theme_url; ?>/img/minus.png" /></a>
-			<a class="add_criterion" href="#" title="<?=lang('lbl_add_criterion'); ?>"><img src="<?=$theme_url; ?>/img/plus.png" /></a>
-		</div>
-	</div><!-- /.tweedee_criterion -->
-	<?php endif; ?>
-
-<?php
-	
-	echo '<div class="submit_wrapper">';
-	echo form_submit(array('name' => 'submit', 'value' => lang('lbl_save_search_criteria'), 'class' => 'submit'));
-	echo '</div>';
-	echo form_close();
-
-?>
+        <tr class="row">
+            <td><?=form_dropdown('search_criteria[0][type]', $criterion_types, $criterion->get_criterion_type()); ?></td>
+            <td><?=form_input('search_criteria[0][value]', $criterion->get_criterion_value()); ?></td>
+            <td class="act">
+                <a class="remove_row btn" href="#" title="<?=lang('lbl_remove_criterion'); ?>"><img src="<?=$theme_url; ?>/img/minus.png" /></a>
+                <a class="add_row" href="#" title="<?=lang('lbl_add_criterion'); ?>"><img src="<?=$theme_url; ?>/img/plus.png" /></a>
+            </td>
+        </tr>
+    <?php
+        endforeach;
+        endif;
+    ?>
+    </tbody>
+</table>
 
 </div><!-- /#tweedee -->
+
+<div class="submit_wrapper"><?=form_submit(array('name' => 'submit', 'value' => lang('lbl_save_search_criteria'), 'class' => 'submit')); ?></div>
+<?=form_close(); ?>
